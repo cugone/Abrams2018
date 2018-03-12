304e600d55454362b5d6298aa0bedc817ee27af7
cugone <cugone@gmail.com>
2018-03-12 03:11:33 -0500

    Applied const-correctness and all functions to operate on Vector2 types.

--- Engine/Code/Engine/Math/Vector2.cpp
+++ Engine/Code/Engine/Math/Vector2.cpp
@@ -1,9 +1,13 @@
 #include "Engine/Math/Vector2.hpp"
 
-Vector2 Vector2::ZERO(0.0f, 0.0f);
-Vector2 Vector2::X_AXIS(1.0f, 0.0f);
-Vector2 Vector2::Y_AXIS(0.0f, 1.0f);
-Vector2 Vector2::ONE(1.0f, 1.0);
+#include <cmath>
+
+#include "Engine/Math/MathUtils.hpp"
+
+const Vector2 Vector2::ZERO(0.0f, 0.0f);
+const Vector2 Vector2::X_AXIS(1.0f, 0.0f);
+const Vector2 Vector2::Y_AXIS(0.0f, 1.0f);
+const Vector2 Vector2::ONE(1.0f, 1.0);
 
 Vector2::Vector2() {
     /* DO NOTHING */
@@ -16,7 +20,7 @@ Vector2::Vector2(float initialX, float initialY)
     /* DO NOTHING */
 }
 
-Vector2 Vector2::operator+(const Vector2& rhs) {
+Vector2 Vector2::operator+(const Vector2& rhs) const {
     return Vector2(x + rhs.x, y + rhs.y);
 }
 
@@ -26,7 +30,7 @@ Vector2& Vector2::operator+=(const Vector2& rhs) {
     return *this;
 }
 
-Vector2 Vector2::operator-(const Vector2& rhs) {
+Vector2 Vector2::operator-(const Vector2& rhs) const {
     return Vector2(x - rhs.x, y - rhs.y);
 }
 
@@ -36,27 +40,45 @@ Vector2& Vector2::operator-=(const Vector2& rhs) {
     return *this;
 }
 
-Vector2 Vector2::operator*(const Vector2& rhs) {
+Vector2 Vector2::operator-() const {
+    return Vector2(-x, -y);
+}
+
+Vector2 Vector2::operator*(const Vector2& rhs) const {
     return Vector2(x * rhs.x, y * rhs.y);
 }
 
-Vector2 Vector2::operator*(float scalar) {
+Vector2 operator*(float lhs, const Vector2& rhs) {
+    return Vector2(lhs * rhs.x, lhs * rhs.y);
+}
+
+Vector2 Vector2::operator*(float scalar) const {
     return Vector2(x * scalar, y * scalar);
 }
 
-Vector2 Vector2::operator*=(float scalar) {
+Vector2& Vector2::operator*=(float scalar) {
     x *= scalar;
     y *= scalar;
     return *this;
 }
 
-Vector2 Vector2::operator*=(const Vector2& rhs) {
+Vector2& Vector2::operator*=(const Vector2& rhs) {
     x *= rhs.x;
     y *= rhs.y;
     return *this;
 }
 
-Vector2 Vector2::operator/(const Vector2& rhs) {
+Vector2 Vector2::operator/(float scalar) const {
+    return Vector2(x / scalar, y / scalar);
+}
+
+Vector2 Vector2::operator/=(float scalar) {
+    x /= scalar;
+    y /= scalar;
+    return *this;
+}
+
+Vector2 Vector2::operator/(const Vector2& rhs) const {
     return Vector2(x / rhs.x, y / rhs.y);
 }
 
@@ -64,4 +86,112 @@ Vector2 Vector2::operator/=(const Vector2& rhs) {
     x /= rhs.x;
     y /= rhs.y;
     return *this;
+}
+
+bool Vector2::operator==(const Vector2& rhs) const {
+    return x == rhs.x && y == rhs.y;
+}
+
+bool Vector2::operator!=(const Vector2& rhs) const {
+    return !(*this == rhs);
+}
+
+void Vector2::GetXY(float& outX, float& outY) const {
+    outX = x;
+    outY = y;
+}
+
+const float* Vector2::GetAsFloatArray() const {
+    return &x;
+}
+
+float Vector2::CalcHeadingRadians() const {
+    return std::atan2(y, x);
+}
+
+float Vector2::CalcLength() const {
+    return std::sqrt(CalcLengthSquared());
+}
+
+float Vector2::CalcLengthSquared() const {
+    return x * x + y * y;
+}
+
+void Vector2::SetHeadingDegrees(float headingDegrees) {
+    SetHeadingRadians(MathUtils::ConvertDegreesToRadians(headingDegrees));
+}
+
+void Vector2::SetHeadingRadians(float headingRadians) {
+    float R = CalcLength();
+    float theta = headingRadians;
+    x = R * std::cos(theta);
+    y = R * std::sin(theta);
+}
+
+void Vector2::SetUnitLengthAndHeadingDegrees(float headingDegrees) {
+    SetUnitLengthAndHeadingRadians(MathUtils::ConvertDegreesToRadians(headingDegrees));
+}
+
+void Vector2::SetUnitLengthAndHeadingRadians(float headingRadians) {
+    Normalize();
+    SetHeadingRadians(headingRadians);
+}
+
+float Vector2::SetLength(float length) {
+    float R = CalcLength();
+    float theta = CalcHeadingRadians();
+    x = length * std::cos(theta);
+    y = length * std::sin(theta);
+    return R;    
+}
+
+void Vector2::SetLengthAndHeadingDegrees(float headingDegrees, float length) {
+    SetLengthAndHeadingRadians(MathUtils::ConvertDegreesToRadians(headingDegrees), length);
+}
+
+void Vector2::SetLengthAndHeadingRadians(float headingRadians, float length) {
+    SetLength(length);
+    SetHeadingRadians(headingRadians);
+}
+
+void Vector2::RotateRadians(float radians) {
+    float R = CalcLength();
+    float old_angle = std::atan2(y, x);
+    float new_angle = old_angle + radians;
+
+    x = R * std::cos(new_angle);
+    y = R * std::sin(new_angle);
+}
+
+float Vector2::Normalize() {
+    float length = CalcLength();
+    if(length > 0.0f) {
+        float inv_length = 1.0f / length;
+        x *= inv_length;
+        y *= inv_length;
+        return length;
+    }
+    return 0.0f;
+}
+
+Vector2 Vector2::GetNormalize() const {
+    float length = CalcLength();
+    if(length > 0.0f) {
+        float inv_length = 1.0f / length;
+        return Vector2(x * inv_length, y * inv_length);
+    }
+    return Vector2::ZERO;
+}
+
+void Vector2::Rotate90Degrees() {
+    SetXY(-y, x);
+}
+
+void Vector2::RotateNegative90Degrees() {
+    SetXY(y, -x);
+}
+
+void Vector2::SetXY(float newX, float newY) {
+    x = newX;
+    y = newY;
 }
--- Engine/Code/Engine/Math/Vector2.hpp
+++ Engine/Code/Engine/Math/Vector2.hpp
@@ -3,10 +3,10 @@
 class Vector2 {
 public:
 
-    static Vector2 ONE;
-    static Vector2 ZERO;
-    static Vector2 X_AXIS;
-    static Vector2 Y_AXIS;
+    static const Vector2 ONE;
+    static const Vector2 ZERO;
+    static const Vector2 X_AXIS;
+    static const Vector2 Y_AXIS;
 
     Vector2();
     Vector2(const Vector2& rhs) = default;
@@ -17,19 +17,52 @@ public:
 
     explicit Vector2(float initialX, float initialY);
 
-    Vector2 operator+(const Vector2& rhs);
+    Vector2 operator+(const Vector2& rhs) const;
     Vector2& operator+=(const Vector2& rhs);
 
-    Vector2 operator-(const Vector2& rhs);
+    Vector2 operator-() const;
+    Vector2 operator-(const Vector2& rhs) const;
     Vector2& operator-=(const Vector2& rhs);
 
-    Vector2 operator*(float scalar);
-    Vector2 operator*=(float scalar);
-    Vector2 operator*(const Vector2& rhs);
-    Vector2 operator*=(const Vector2& rhs);
+    friend Vector2 operator*(float lhs, const Vector2& rhs);
+    Vector2 operator*(float scalar) const;
+    Vector2& operator*=(float scalar);
+    Vector2 operator*(const Vector2& rhs) const;
+    Vector2& operator*=(const Vector2& rhs);
 
-    Vector2 operator/(const Vector2& rhs);
+    Vector2 operator/(float scalar) const;
+    Vector2 operator/=(float scalar);
+    Vector2 operator/(const Vector2& rhs) const;
     Vector2 operator/=(const Vector2& rhs);
+
+    bool operator==(const Vector2& rhs) const;
+    bool operator!=(const Vector2& rhs) const;
+
+    void GetXY(float& outX, float& outY) const;
+    const float* GetAsFloatArray() const;
+
+    float CalcHeadingRadians() const;
+    float CalcLength() const;
+    float CalcLengthSquared() const;
+
+
+    void SetHeadingDegrees(float headingDegrees);
+    void SetHeadingRadians(float headingRadians);
+
+    void SetUnitLengthAndHeadingDegrees(float headingDegrees);
+    void SetUnitLengthAndHeadingRadians(float headingRadians);
+    float SetLength(float length);
+    void SetLengthAndHeadingDegrees(float headingDegrees, float length);
+    void SetLengthAndHeadingRadians(float headingRadians, float length);
+
+    float Normalize();
+    Vector2 GetNormalize() const;
+
+    void Rotate90Degrees();
+    void RotateNegative90Degrees();
+    void RotateRadians(float radians);
+
+    void SetXY(float newX, float newY);
 
     float x;
     float y;

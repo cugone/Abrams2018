560bb87b73fd0855e092f993b16b1a2e7425cec8
cugone <cugone@gmail.com>
2018-03-12 03:05:31 -0500

    Added MathUtils with Random functions, Radian converters, and Vector2 operations

---
+++ Engine/Code/Engine/Math/MathUtils.cpp
@@ -0,0 +1,182 @@
+#include "Engine/Math/MathUtils.hpp"
+
+#include <cmath>
+
+namespace MathUtils {
+
+namespace {
+unsigned int MT_RANDOM_SEED = 0;
+}
+
+void SetRandomEngineSeed(unsigned int seed) {
+    MT_RANDOM_SEED = seed;
+}
+
+float ConvertDegreesToRadians(float degrees) {
+    return degrees * (MathUtils::M_PI / 180.0f);
+}
+
+float ConvertRadiansToDegrees(float radians) {
+    return radians * (180.0f * MathUtils::M_1_PI);
+}
+
+//NOT THREAD SAFE
+std::random_device& GetRandomDevice() {
+    static std::random_device rd;
+    return rd;
+}
+
+//NOT THREAD SAFE
+std::mt19937& GetMTRandomEngine(unsigned int seed /*= 0*/) {
+    static std::mt19937 e = std::mt19937(!seed ? GetRandomDevice()() : seed);
+    return e;
+}
+
+//NOT THREAD SAFE
+std::mt19937_64& GetMT64RandomEngine(unsigned int seed /*= 0*/) {
+    static std::mt19937_64 e = std::mt19937_64(!seed ? GetRandomDevice()() : seed);
+    return e;
+}
+
+int GetRandomIntLessThan(int maxValueNotInclusive) {
+    std::uniform_int_distribution<int> d(0, maxValueNotInclusive - 1);
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+int GetRandomIntInRange(int minInclusive, int maxInclusive) {
+    std::uniform_int_distribution<int> d(minInclusive, maxInclusive);
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+
+}
+
+long GetRandomLongLessThan(long maxValueNotInclusive) {
+    std::uniform_int_distribution<long> d(0L, maxValueNotInclusive - 1L);
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+long GetRandomLongInRange(long minInclusive, long maxInclusive) {
+    std::uniform_int_distribution<long> d(minInclusive, maxInclusive);
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+
+}
+
+float GetRandomFloatInRange(float minInclusive, float maxInclusive) {
+    std::uniform_real_distribution<float> d(minInclusive, std::nextafter(maxInclusive, maxInclusive + 1.0f));
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+float GetRandomFloatZeroToOne() {
+    std::uniform_real_distribution<float> d(0.0f, std::nextafter(1.0f, 2.0f));
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+float GetRandomFloatZeroUpToOne() {
+    std::uniform_real_distribution<float> d(0.0f, 1.0f);
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+float GetRandomFloatNegOneToOne() {
+    return GetRandomFloatInRange(-1.0f, 1.0f);
+}
+
+double GetRandomDoubleInRange(double minInclusive, double maxInclusive) {
+    std::uniform_real_distribution<double> d(minInclusive, std::nextafter(maxInclusive, maxInclusive + 1.0));
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+double GetRandomDoubleZeroToOne() {
+    std::uniform_real_distribution<double> d(0.0, std::nextafter(1.0, 2.0));
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+double GetRandomDoubleZeroUpToOne() {
+    std::uniform_real_distribution<double> d(0.0, 1.0);
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+double GetRandomDoubleNegOneToOne() {
+    return GetRandomDoubleInRange(-1.0, 1.0);
+}
+
+long double GetRandomLongDoubleInRange(long double minInclusive, long double maxInclusive) {
+    std::uniform_real_distribution<long double> d(minInclusive, std::nextafter(maxInclusive, maxInclusive + 1.0L));
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+long double GetRandomLongDoubleZeroToOne() {
+    std::uniform_real_distribution<long double> d(0.0L, std::nextafter(1.0L, 2.0L));
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+long double GetRandomLongDoubleZeroUpToOne() {
+    std::uniform_real_distribution<long double> d(0.0L, 1.0L);
+    return d(GetMTRandomEngine(MT_RANDOM_SEED));
+}
+
+long double GetRandomLongDoubleNegOneToOne() {
+    return GetRandomDoubleInRange(-1.0L, 1.0L);
+}
+
+bool IsPercentChance(float probability) {
+    float roll = GetRandomFloatZeroToOne();
+    return roll < probability;
+}
+
+bool IsPercentChance(double probability) {
+    float roll = GetRandomDoubleZeroToOne();
+    return roll < probability;
+}
+
+bool IsPercentChance(long double probability) {
+    float roll = GetRandomLongDoubleZeroToOne();
+    return roll < probability;
+}
+
+float CosDegrees(float degrees) {
+    float radians = MathUtils::ConvertDegreesToRadians(degrees);
+    return std::cos(radians);
+}
+
+float SinDegrees(float degrees) {
+    float radians = MathUtils::ConvertDegreesToRadians(degrees);
+    return std::sin(radians);
+}
+
+float Atan2Degrees(float y, float x) {
+    float radians = std::atan2(y, x);
+    return MathUtils::ConvertRadiansToDegrees(radians);
+}
+
+bool IsEquivalent(float a, float b, float epsilon /*= 0.00001f*/) {
+    return std::abs(a - b) < epsilon;
+}
+
+bool IsEquivalent(double a, double b, double epsilon /*= 0.0001*/) {
+    return std::abs(a - b) < epsilon;
+}
+
+bool IsEquivalent(long double a, long double b, long double epsilon /*= 0.0001L*/) {
+    return std::abs(a - b) < epsilon;
+}
+
+bool IsEquivalent(const Vector2& a, const Vector2& b, float epsilon /*= 0.0001f*/) {
+    return IsEquivalent(a.x, b.x, epsilon) && IsEquivalent(a.y, b.y, epsilon);
+}
+
+float DotProduct(const Vector2& a, const Vector2& b) {
+    return a.x * b.x + a.y * b.y;
+}
+
+Vector2 Project(const Vector2& a, const Vector2& b) {
+    return (DotProduct(a, b) / DotProduct(b, b)) * b;
+}
+
+Vector2 Reflect(const Vector2& in, const Vector2& normal) {
+    return in - ((2.0f * DotProduct(in, normal)) * normal);
+}
+
+Vector2 ProjectAlongPlane(const Vector2& v, const Vector2& n) {
+    return v - (DotProduct(v, n) * n);
+}
+
+} //End MathUtils
---
+++ Engine/Code/Engine/Math/MathUtils.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <algorithm>
+#include <random>
+#include <utility>
+
+#include "Engine/Math/Vector2.hpp"
+
+namespace MathUtils {
+
+constexpr const float M_PI = 3.14159265358979323846f;                             // pi
+constexpr const float M_E = 2.71828182845904523536f;                              // e
+constexpr const float M_LOG2E = 1.44269504088896340736f;                          // log2(e)
+constexpr const float M_LOG10E = 0.43429448190325182765f;                         // log10(e)
+constexpr const float M_LN2 = 0.69314718055994530942f;                            // ln(2)
+constexpr const float M_LN10 = 2.30258509299404568402f;                           // ln(10)
+constexpr const float M_PI_2 = 1.57079632679489661923f;                           // pi/2
+constexpr const float M_PI_4 = 0.78539816339744830962f;                           // pi/4
+constexpr const float M_1_PI = 0.31830988618379067151f;                           // 1/pi
+constexpr const float M_2_PI = 0.63661977236758134308f;                           // 2/pi
+constexpr const float M_2_SQRTPI = 1.12837916709551257390f;                       // 2/sqrt(pi)
+constexpr const float M_SQRT2 = 1.41421356237309504880f;                          // sqrt(2)
+constexpr const float M_1_SQRT2 = 0.70710678118654752440f;                        // 1/sqrt(2)
+constexpr const float M_SQRT3_3 = 0.57735026918962576451f;                        // sqrt(3)/3
+constexpr const float M_TAU = 1.61803398874989484821f;                            // tau (golden ratio)
+constexpr const long double KIB_BYTES_RATIO = 0.0009765625;                       // Kilobyte/Bytes
+constexpr const long double MIB_BYTES_RATIO = 0.00000095367431640625;             // Megabyte/Bytes
+constexpr const long double GIB_BYTES_RATIO = 0.000000000931322574615478515625;   // Gigabyte/Bytes
+constexpr const long double BYTES_KIB_RATIO = 1024.0;                             // Bytes/Kilobytes
+constexpr const long double BYTES_MIB_RATIO = 1048576.0;                          // Bytes/Megabytes
+constexpr const long double BYTES_GIB_RATIO = 1073741824.0;                       // Bytes/Gigabytes
+
+//NOT THREAD SAFE
+void SetRandomEngineSeed(unsigned int seed);
+//NOT THREAD SAFE
+std::random_device& GetRandomDevice();
+//NOT THREAD SAFE
+std::mt19937& GetMTRandomEngine(unsigned int seed = 0);
+//NOT THREAD SAFE
+std::mt19937_64& GetMT64RandomEngine(unsigned int seed = 0);
+
+float ConvertDegreesToRadians(float degrees);
+float ConvertRadiansToDegrees(float radians);
+
+int GetRandomIntLessThan(int maxValueNotInclusive);
+int GetRandomIntInRange(int minInclusive, int maxInclusive);
+
+long GetRandomLongLessThan(long maxValueNotInclusive);
+long GetRandomLongInRange(long minInclusive, long maxInclusive);
+
+float GetRandomFloatInRange(float minInclusive, float maxInclusive);
+float GetRandomFloatZeroToOne();
+float GetRandomFloatZeroUpToOne();
+float GetRandomFloatNegOneToOne();
+bool IsPercentChance(float probability);
+
+double GetRandomDoubleInRange(double minInclusive, double maxInclusive);
+double GetRandomDoubleZeroToOne();
+double GetRandomDoubleZeroUpToOne();
+double GetRandomDoubleNegOneToOne();
+bool IsPercentChance(double probability);
+
+long double GetRandomLongDoubleInRange(long double minInclusive, long double maxInclusive);
+long double GetRandomLongDoubleZeroToOne();
+long double GetRandomLongDoubleZeroUpToOne();
+long double GetRandomLongDoubleNegOneToOne();
+bool IsPercentChance(long double probability);
+
+
+float CosDegrees(float degrees);
+float SinDegrees(float degrees);
+float Atan2Degrees(float y, float x);
+
+bool IsEquivalent(float a, float b, float epsilon = 0.00001f);
+bool IsEquivalent(double a, double b, double epsilon = 0.0001);
+bool IsEquivalent(long double a, long double b, long double epsilon = 0.0001L);
+bool IsEquivalent(const Vector2& a, const Vector2& b, float epsilon = 0.0001f);
+
+float DotProduct(const Vector2& a, const Vector2& b);
+
+Vector2 Project(const Vector2& a, const Vector2& b);
+
+Vector2 Reflect(const Vector2& in, const Vector2& normal);
+
+}

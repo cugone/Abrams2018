fa1831a8eb431bbb3c61530ce472546b739ae26f
cugone <cugone@gmail.com>
2018-03-18 13:36:14 -0500

    Removed extraneous code Removed unused parts of main, reordered functions for readability

--- RendererTests/Code/Game/Main_Win32.cpp
+++ RendererTests/Code/Game/Main_Win32.cpp
@@ -7,30 +7,11 @@
 
 #include "Game/GameCommon.hpp"
 
-HWND _hwnd = nullptr;
-WNDCLASSEX _wc{};
-bool _isQuitting = false;
-int window_width = 800;
-int window_height = 600;
-
-bool CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void Initialize(HINSTANCE hInstance, LPWSTR lpCmdLine, int nShowCmd);
 void Shutdown();
 void RunMessagePump();
 
-void RunMessagePump() {
-    MSG msg{};
-    for(;;) {
-        const BOOL hasMsg = ::PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE);
-        if(!hasMsg) {
-            break;
-        }
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-}
-
-int WINAPI wWinMain([[maybe_unused]] HINSTANCE hInstance, [[maybe_unused]] HINSTANCE hPrevInstance, [[maybe_unused]] LPWSTR lpCmdLine, [[maybe_unused]] int nShowCmd) {
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPWSTR lpCmdLine, int nShowCmd) {
 
     Initialize(hInstance, lpCmdLine, nShowCmd);
 
@@ -44,8 +25,20 @@ int WINAPI wWinMain([[maybe_unused]] HINSTANCE hInstance, [[maybe_unused]] HINST
 }
 
 
-void Initialize([[maybe_unused]] HINSTANCE hInstance, [[maybe_unused]] LPWSTR lpCmdLine, [[maybe_unused]] int nShowCmd) {
+void Initialize(HINSTANCE /*hInstance*/, LPWSTR /*lpCmdLine*/, int /*nShowCmd*/) {
     g_theApp = new App();
+}
+
+void RunMessagePump() {
+    MSG msg{};
+    for(;;) {
+        const BOOL hasMsg = ::PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE);
+        if(!hasMsg) {
+            break;
+        }
+        ::TranslateMessage(&msg);
+        ::DispatchMessage(&msg);
+    }
 }
 
 void Shutdown() {

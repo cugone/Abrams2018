0a8d5d1ff187633058a945d412e808d83d6311d4
cugone <cugone@gmail.com>
2018-06-12 21:45:47 -0500

    Implemented AnimatedSprite;

---
+++ Engine/Code/Engine/Renderer/AnimatedSprite.cpp
@@ -0,0 +1,240 @@
+#include "Engine/Renderer/AnimatedSprite.hpp"
+
+#include "Engine/Core/StringUtils.hpp"
+
+#include "Engine/Math/MathUtils.hpp"
+
+#include "Engine/Renderer/Renderer.hpp"
+#include "Engine/Renderer/SpriteSheet.hpp"
+#include "Engine/Renderer/Texture.hpp"
+#include "Engine/Renderer/Texture2D.hpp"
+
+AnimatedSprite::AnimatedSprite(SpriteSheet* spriteSheet,
+                               float durationSeconds,
+                               int startSpriteIndex,
+                               int frameLength,
+                               SpriteAnimMode playbackMode /*= SpriteAnimMode::LOOPING*/)
+    : _sheet(spriteSheet)
+    , _duration_seconds(durationSeconds)
+    , _playback_mode(playbackMode)
+    , _start_index(startSpriteIndex)
+    , _end_index(startSpriteIndex + frameLength)
+{
+    bool has_frames = frameLength > 0;
+    _max_frame_delta_seconds = _duration_seconds / (has_frames ? (_end_index - _start_index) : 1);
+}
+
+AnimatedSprite::AnimatedSprite(Renderer& renderer, const XMLElement& elem) {
+    LoadFromXml(renderer, elem);
+}
+
+AnimatedSprite::~AnimatedSprite() {
+    delete _sheet;
+    _sheet = nullptr;
+}
+
+void AnimatedSprite::Update(float deltaSeconds) {
+
+    _elapsed_frame_delta_seconds += deltaSeconds;
+    while(_elapsed_frame_delta_seconds >= _max_frame_delta_seconds) {
+        _elapsed_frame_delta_seconds -= _max_frame_delta_seconds;
+    }
+    switch(_playback_mode) {
+        case SpriteAnimMode::LOOPING:
+            if(IsFinished()) {
+                while(_elapsed_seconds >= _duration_seconds) {
+                    _elapsed_seconds -= _duration_seconds;
+                }
+            }
+            break;
+        case SpriteAnimMode::LOOPING_REVERSE:
+            if(IsFinished()) {
+                _elapsed_seconds = _duration_seconds;
+                deltaSeconds *= -1.0f;
+            }
+            break;
+        case SpriteAnimMode::PLAY_TO_BEGINNING:
+            if(IsFinished()) {
+                _is_playing = false;
+                _elapsed_seconds = 0.0f;
+            }
+            break;
+        case SpriteAnimMode::PLAY_TO_END:
+            if(IsFinished()) {
+                _is_playing = false;
+                _elapsed_seconds = _duration_seconds;
+            }
+            break;
+        case SpriteAnimMode::PINGPONG:
+            if(_elapsed_seconds < 0.0) {
+                deltaSeconds *= -1.0f;
+                while(_elapsed_seconds < 0.0f) {
+                    _elapsed_seconds += _duration_seconds;
+                }
+            } else if(_elapsed_seconds >= _duration_seconds) {
+                deltaSeconds *= -1.0f;
+                while(_elapsed_seconds >= _duration_seconds) {
+                    _elapsed_seconds -= _duration_seconds;
+                }
+            }
+            break;
+        default:
+            break;
+    }
+    _elapsed_seconds += deltaSeconds;
+}
+
+AABB2 AnimatedSprite::GetCurrentTexCoords() const {
+
+    auto framesPerSecond = 1.0f / _max_frame_delta_seconds;
+    auto frameIndex = static_cast<int>(framesPerSecond * _elapsed_seconds);
+    auto length = _end_index - _start_index;
+    switch(_playback_mode) {
+        case SpriteAnimMode::PLAY_TO_END:
+            if(frameIndex >= length - 1) {
+                frameIndex = _end_index;
+            }
+            break;
+        case SpriteAnimMode::PLAY_TO_BEGINNING:
+            if(frameIndex <= 0) {
+                frameIndex = 0;
+            }
+            break;
+        case SpriteAnimMode::LOOPING: /* FALLTHROUGH */
+        case SpriteAnimMode::LOOPING_REVERSE:
+            if(frameIndex >= length - 1) {
+                frameIndex = 0;
+            }
+            if(frameIndex <= 0) {
+                frameIndex = _end_index;
+            }
+            break;
+        case SpriteAnimMode::PINGPONG:
+            if(frameIndex >= length - 1) {
+                frameIndex = _end_index;
+            }
+            if(frameIndex <= 0) {
+                frameIndex = 0;
+            }
+            break;
+        default:
+            break;
+    }
+
+    return _sheet->GetTexCoordsFromSpriteIndex(frameIndex);
+}
+
+const Texture* const AnimatedSprite::GetTexture() const {
+    return &_sheet->GetTexture();
+}
+
+void AnimatedSprite::TogglePause() {
+    _is_playing = !_is_playing;
+}
+
+void AnimatedSprite::Pause() {
+    _is_playing = false;
+}
+
+void AnimatedSprite::Resume() {
+    _is_playing = true;
+}
+
+void AnimatedSprite::Reset() {
+    _elapsed_seconds = 0.0f;
+}
+
+bool AnimatedSprite::IsFinished() const {
+    if(!_is_playing) {
+        return false;
+    }
+    switch(_playback_mode) {
+        case SpriteAnimMode::LOOPING: /* FALL THROUGH */
+        case SpriteAnimMode::PLAY_TO_END:
+            return !(_elapsed_seconds < _duration_seconds);
+        case SpriteAnimMode::LOOPING_REVERSE: /* FALL THROUGH */
+        case SpriteAnimMode::PLAY_TO_BEGINNING:
+            return _elapsed_seconds < 0.0f;
+        case SpriteAnimMode::PINGPONG:
+            return false;
+        default:
+            return !(_elapsed_seconds < _duration_seconds);
+    }
+}
+
+bool AnimatedSprite::IsPlaying() const {
+    return _is_playing;
+}
+
+float AnimatedSprite::GetDurationSeconds() const {
+    return _duration_seconds;
+}
+
+float AnimatedSprite::GetSecondsElapsed() const {
+    return _elapsed_seconds;
+}
+
+float AnimatedSprite::GetSecondsRemaining() const {
+    return _duration_seconds - _elapsed_seconds;
+}
+
+float AnimatedSprite::GetFractionElapsed() const {
+    return _elapsed_seconds / _duration_seconds;
+}
+
+float AnimatedSprite::GetFractionRemaining() const {
+    return (_duration_seconds - _elapsed_seconds) / _duration_seconds;
+}
+
+void AnimatedSprite::SetSecondsElapsed(float secondsElapsed) {
+    _elapsed_seconds = secondsElapsed;
+}
+
+void AnimatedSprite::SetFractionElapsed(float fractionElapsed) {
+    _elapsed_seconds = _duration_seconds * fractionElapsed;
+}
+
+AnimatedSprite::SpriteAnimMode AnimatedSprite::GetAnimModeFromOptions(bool looping, bool backwards, bool ping_pong /*= false*/) {
+    if(ping_pong) {
+        return SpriteAnimMode::PINGPONG;
+    }
+
+    if(looping) {
+        if(backwards) {
+            return SpriteAnimMode::LOOPING_REVERSE;
+        }
+        return SpriteAnimMode::LOOPING;
+    }
+    if(backwards) {
+        return SpriteAnimMode::PLAY_TO_BEGINNING;
+    }
+    return SpriteAnimMode::PLAY_TO_END;
+}
+
+void AnimatedSprite::LoadFromXml(Renderer& renderer, const XMLElement& elem) {
+    DataUtils::ValidateXmlElement(elem, "animation", "spritesheet,animationset", "", "", "name");
+    auto xml_sheet = elem.FirstChildElement("spritesheet");
+    DataUtils::ValidateXmlElement(*xml_sheet, "spritesheet", "", "src,dimensions");
+    _sheet = renderer.CreateSpriteSheet(*xml_sheet);
+
+    auto xml_animset = elem.FirstChildElement("animationset");
+    DataUtils::ValidateXmlElement(*xml_animset, "animationset", "", "startindex,framelength,duration", "", "loop,reverse,pingpong");
+
+    _start_index = DataUtils::ParseXmlAttribute(*xml_animset, "startindex", _start_index);
+    int frameLength = DataUtils::ParseXmlAttribute(*xml_animset, "framelength", 0);
+    _end_index = _start_index + frameLength;
+
+    auto min_duration = 0.016f;
+    _duration_seconds = DataUtils::ParseXmlAttribute(*xml_animset, "duration", _duration_seconds);
+    if(_duration_seconds < min_duration) {
+        _duration_seconds = min_duration;
+    }
+
+    bool is_looping = DataUtils::ParseXmlAttribute(*xml_animset, "loop", false);
+    bool is_reverse = DataUtils::ParseXmlAttribute(*xml_animset, "reverse", false);
+    bool is_pingpong = DataUtils::ParseXmlAttribute(*xml_animset, "pingpong", false);
+    _playback_mode = GetAnimModeFromOptions(is_looping, is_reverse, is_pingpong);
+
+    bool has_frames = frameLength > 0;
+    _max_frame_delta_seconds = _duration_seconds / (has_frames ? (_end_index - _start_index) : 1);
+}
---
+++ Engine/Code/Engine/Renderer/AnimatedSprite.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Engine/Core/DataUtils.hpp"
+
+#include "Engine/Math/AABB2.hpp"
+
+class Renderer;
+class SpriteSheet;
+class Texture;
+
+class AnimatedSprite {
+public:
+    enum class SpriteAnimMode : int {
+        PLAY_TO_END,	   // Play from time=0 to durationSeconds, then finish
+        PLAY_TO_BEGINNING, // Play from time=durationSeconds to 0, then finish
+        LOOPING,		   // Play from time=0 to end then repeat (never finish)
+        LOOPING_REVERSE,   // Play from time=durationSeconds then repeat (never finish)
+        PINGPONG, 	       // Play forwards, backwards, forwards, backwards...
+        NUM_SPRITE_ANIM_MODES,
+    };
+
+    AnimatedSprite(SpriteSheet* spriteSheet, float durationSeconds,
+                   int startSpriteIndex, int frameLength, SpriteAnimMode playbackMode = SpriteAnimMode::LOOPING);
+    AnimatedSprite(Renderer& renderer, const XMLElement& elem);
+    ~AnimatedSprite();
+
+    void Update(float deltaSeconds);
+    AABB2 GetCurrentTexCoords() const;	               // Based on the current elapsed time
+    const Texture* const GetTexture() const;
+    void TogglePause();
+    void Pause();					                   // Starts unpaused (playing) by default
+    void Resume();				                       // Resume after pausing
+    void Reset();					                   // Rewinds to time 0 and starts (re)playing
+    bool IsFinished() const;                           //{ return m_isFinished; }
+    bool IsPlaying() const;                            //{ return m_isPlaying; }
+    float GetDurationSeconds() const;                  //{ return m_durationSeconds; }
+    float GetSecondsElapsed() const;                   //{ return m_elapsedSeconds; }
+    float GetSecondsRemaining() const;
+    float GetFractionElapsed() const;
+    float GetFractionRemaining() const;
+    void SetSecondsElapsed(float secondsElapsed);	   // Jump to specific time
+    void SetFractionElapsed(float fractionElapsed);    // e.g. 0.33f for one-third in
+
+protected:
+private:
+
+    void LoadFromXml(Renderer& renderer, const XMLElement& elem);
+    SpriteAnimMode GetAnimModeFromOptions(bool looping, bool backwards, bool ping_pong /*= false*/);
+
+    SpriteSheet* _sheet;
+    float _duration_seconds = 0.016f;
+    float _elapsed_seconds = 0.0f;
+    float _elapsed_frame_delta_seconds = 0.0f;
+    float _max_frame_delta_seconds = 0.0f;
+    SpriteAnimMode _playback_mode = SpriteAnimMode::LOOPING;
+    int _start_index = 0;
+    int _end_index = 1;
+    bool _is_playing = true;
+
+};

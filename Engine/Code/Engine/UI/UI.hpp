ebd0e119479dcb1e7c6e83651022f4f2102d206f
cugone <cugone@gmail.com>
2018-07-11 13:22:50 -0500

    Added UI::Text include;

--- Engine/Code/Engine/UI/UI.hpp
+++ Engine/Code/Engine/UI/UI.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "Engine/UI/Types.hpp"
-#include "Engine/UI/Element.hpp"
 #include "Engine/UI/Canvas.hpp"
+#include "Engine/UI/Element.hpp"
 #include "Engine/UI/Panel.hpp"
+#include "Engine/UI/Text.hpp"
+#include "Engine/UI/Types.hpp"

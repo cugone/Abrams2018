a0a0cc3f593ee7388937e480e775de6520e55450
cugone <cugone@gmail.com>
2018-10-31 23:24:07 -0500

    Refactored Date/Time stamp functions;

--- Engine/Code/Engine/Core/TimeUtils.cpp
+++ Engine/Code/Engine/Core/TimeUtils.cpp
@@ -13,25 +13,13 @@ std::string GetDateTimeStampFromNow(const DateTimeStampOptions& options /*= Date
     std::tm tm;
     ::localtime_s(&tm, &t);
     std::stringstream msg;
-    std::string fmt = "%Y%m%d%I%M%S";
-    if(options.use_24_hour_clock) {
-        fmt = "%Y%m%d%H%M%S";
-    }
-    if(options.use_separator) {
-        fmt.insert(fmt.begin() + 2, '-');
-        fmt.insert(fmt.begin() + 5, '-');
-        fmt.insert(fmt.begin() + 8, ' ');
-        fmt.insert(fmt.begin() + 11, ':');
-        fmt.insert(fmt.begin() + 14, ':');
-        if(options.include_milliseconds) {
-            fmt.insert(fmt.end(), '.');
-        }
-    }
+    std::string fmt = options.use_24_hour_clock ? (options.use_separator ? "%Y-%m-%d %H:%M:%S" : "%Y%m%d%H%M%S")
+                                                : (options.use_separator ? "%Y-%m-%d %I:%M:%S" : "%Y%m%d%I%M%S");
     msg << std::put_time(&tm, fmt.c_str());
 
     if(options.include_milliseconds) {
         auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
-        msg << std::fixed << std::setw(3) << std::setfill('0') << ms.count();
+        msg << '.' << std::fixed << std::setw(3) << std::setfill('0') << ms.count();
     }
 
     return msg.str();
@@ -43,23 +31,14 @@ std::string GetTimeStampFromNow(const DateTimeStampOptions& options /*= DateTime
     auto t = system_clock::to_time_t(now);
     std::tm tm;
     ::localtime_s(&tm, &t);
-    std::stringstream msg;
-    std::string fmt = "%I%M%S";
-    if(options.use_24_hour_clock) {
-        fmt = "%H%M%S";
-    }
-    if(options.use_separator) {
-        fmt.insert(fmt.begin() + 2, ':');
-        fmt.insert(fmt.begin() + 5, ':');
-        if(options.include_milliseconds) {
-            fmt.insert(fmt.end() - 1, '.');
-        }
-    }
+    std::ostringstream msg;
+    std::string fmt = options.use_24_hour_clock ? (options.use_separator ? "%H:%M:%S" : "%H%M%S")
+                                                : (options.use_separator ? "%I:%M:%S" : "%I%M%S");
     msg << std::put_time(&tm, fmt.c_str());
 
     if(options.include_milliseconds) {
         auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
-        msg << ms.count();
+        msg << '.' << std::fixed << std::setw(3) << std::setfill('0') << ms.count();
     }
 
     return msg.str();
@@ -72,7 +51,8 @@ std::string GetDateStampFromNow(const DateTimeStampOptions& options /*= DateTime
     std::tm tm;
     ::localtime_s(&tm, &t);
     std::stringstream msg;
-    msg << std::put_time(&tm, options.use_separator ? "%Y-%m-%d" : "%Y%m%d");
+    std::string fmt = options.use_separator ? "%Y-%m-%d" : "%Y%m%d";
+    msg << std::put_time(&tm, fmt.c_str());
     return msg.str();
 }
 

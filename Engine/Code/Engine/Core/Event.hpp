5d5601ab663a1fb9b65df491ab496e99c1f82779
cugone <cugone@gmail.com>
2018-11-09 14:19:58 -0600

    perfect forwarding of arguments;

--- Engine/Code/Engine/Core/Event.hpp
+++ Engine/Code/Engine/Core/Event.hpp
@@ -2,12 +2,12 @@
 
 #include <vector>
 
-template <typename ...ARGS>
+template <typename... ARGS>
 class Event {
 public:
     struct event_sub_t;
-    using cb_t = void(*)(event_sub_t*, ARGS...);
-    using cb_with_arg_t = void(*)(void*, ARGS...);
+    using cb_t = void(*)(event_sub_t*, ARGS&&...);
+    using cb_with_arg_t = void(*)(void*, ARGS&&...);
 
     struct event_sub_t {
         cb_t cb;
@@ -45,7 +45,7 @@ public:
     }
 
     template <typename T>
-    void Subscribe_method(T *obj, void (T::*mcb)(ARGS...)) {
+    void Subscribe_method(T *obj, void (T::*mcb)(ARGS&&...)) {
         event_sub_t sub;
         sub.cb = MethodCallback<T, decltype(mcb)>;
         sub.secondary_cb = *(void**)(&mcb);
@@ -54,7 +54,7 @@ public:
     }
 
     template <typename T>
-    void Unsubscribe_method(T *obj, void (T::*mcb)(ARGS...)) {
+    void Unsubscribe_method(T *obj, void (T::*mcb)(ARGS&&...)) {
         Unsubscribe(obj, *(void**)&mcb);
     }
 
@@ -63,32 +63,32 @@ public:
         Unsubscribe_by_argument(obj);
     }
 
-    void Trigger(ARGS ...args) {
+    void Trigger(ARGS&&... args) {
         for(auto& sub : subscriptions) {
-            sub.cb(&sub, args...);
+            sub.cb(&sub, std::forward<ARGS>(args)...);
         }
     }
 
 private:
     std::vector<event_sub_t> subscriptions;
 
-    static void FunctionWithArgumentCallback(event_sub_t *sub, ARGS ...args);
+    static void FunctionWithArgumentCallback(event_sub_t *sub, ARGS&&... args);
 
     template <typename T, typename MCB>
-    static void MethodCallback(event_sub_t *sub, ARGS ...args);
+    static void MethodCallback(event_sub_t *sub, ARGS&&... args);
 
 };
 
 template <typename ...ARGS>
-void Event<ARGS...>::FunctionWithArgumentCallback(event_sub_t *sub, ARGS ...args) {
+void Event<ARGS...>::FunctionWithArgumentCallback(event_sub_t *sub, ARGS&&... args) {
     cb_with_arg_t cb = (cb_with_arg_t)(sub->secondary_cb);
-    cb(sub->user_arg, args...);
+    cb(sub->user_arg, std::forward<ARGS>(args)...);
 }
 
 template <typename ...ARGS>
 template <typename T, typename MCB>
-void Event<ARGS...>::MethodCallback(event_sub_t *sub, ARGS ...args) {
+void Event<ARGS...>::MethodCallback(event_sub_t *sub, ARGS&&... args) {
     MCB mcb = *(MCB*)&(sub->secondary_cb);
     T *obj = (T*)(sub->user_arg);
-    (obj->*mcb)(args...);
+    (obj->*mcb)(std::forward<ARGS>(args)...);
 }

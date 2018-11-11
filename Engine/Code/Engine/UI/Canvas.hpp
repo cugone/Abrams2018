761c5a46934d77bf94edb72ebc3682378df552bf
cugone <cugone@gmail.com>
2018-11-11 10:50:21 -0600

    Added CreateCHild overloads;

--- Engine/Code/Engine/UI/Canvas.hpp
+++ Engine/Code/Engine/UI/Canvas.hpp
@@ -17,6 +17,20 @@ public:
     void SetupMVPFromTargetAndCamera(Renderer* renderer) const;
     virtual void DebugRender(Renderer* renderer) const override;
     const Camera2D* GetUICamera() const;
+
+    template<typename T>
+    T* CreateChild();
+    template<typename T, typename ...Args>
+    T* CreateChild(Args&&... args);
+    template<typename T>
+    T* CreateChildBefore(UI::Element* youngerSibling);
+    template<typename T, typename ...Args>
+    T* CreateChildBefore(UI::Element* youngerSibling, Args&&... args);
+    template<typename T>
+    T* CreateChildAfter(UI::Element* olderSibling);
+    template<typename T, typename ...Args>
+    T* CreateChildAfter(UI::Element* olderSibling, Args&&... args);
+
 protected:
 private:
     void CalcDimensionsAndAspectRatio(Vector2& dimensions, float& aspectRatio);
@@ -28,5 +42,34 @@ private:
     float _reference_resolution = 0.0f;
     float _aspect_ratio = 1.0f;
 };
+
+template<typename T>
+T* UI::Canvas::CreateChild() {
+    return dynamic_cast<T*>(Element::CreateChild<T>(this));
+}
+
+template<typename T, typename ...Args>
+T* UI::Canvas::CreateChild(Args&&... args) {
+    return dynamic_cast<T*>(Element::CreateChild<T>(this, std::forward<Args>(args)...));
+}
+
+template<typename T>
+T* UI::Canvas::CreateChildBefore(UI::Element* youngerSibling) {
+    return dynamic_cast<T*>(Element::CreateChildBefore<T>(this, youngerSibling));
+}
+
+template<typename T, typename ...Args>
+T* UI::Canvas::CreateChildBefore(UI::Element* youngerSibling, Args&&... args) {
+    return dynamic_cast<T*>(Element::CreateChildBefore<T>(this, youngerSibling, std::forward<Args>(args)...));
+}
+
+template<typename T>
+T* UI::Canvas::CreateChildAfter(UI::Element* olderSibling) {
+    return dynamic_cast<T*>(Element::CreateChildAfter<T>(this, olderSibling));
+}
+template<typename T, typename ...Args>
+T* UI::Canvas::CreateChildAfter(UI::Element* olderSibling, Args&&... args) {
+    return dynamic_cast<T*>(Element::CreateChildAfter<T>(this, olderSibling, std::forward<Args>(args)...));
+}
 
 } //End UI

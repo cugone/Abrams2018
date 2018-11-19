d530a21aae7311e2be2f31028f42f08f1104f3c4
cugone <cugone@gmail.com>
2018-11-18 21:56:11 -0600

    Removed universal references;

--- Engine/Code/Engine/Core/Event.hpp
+++ Engine/Code/Engine/Core/Event.hpp
@@ -6,8 +6,8 @@ template <typename... ARGS>
 class Event {
 public:
     struct event_sub_t;
-    using cb_t = void(*)(event_sub_t*, ARGS&&...);
-    using cb_with_arg_t = void(*)(void*, ARGS&&...);
+    using cb_t = void(*)(event_sub_t*, ARGS...);
+    using cb_with_arg_t = void(*)(void*, ARGS...);
 
     struct event_sub_t {
         cb_t cb;
@@ -28,24 +28,24 @@ public:
 
     void Unsubscribe(void *user_arg, void* cb) {
         subscriptions.erase(std::remove_if(std::begin(subscriptions),
-                                           std::end(subscriptions),
-                                           [&cb, &user_arg](const event_sub_t& sub) {
-                                               return (sub.secondary_cb == cb) && (sub.user_arg == user_arg);
-                                           }),
-                            std::end(subscriptions));
+            std::end(subscriptions),
+            [&cb, &user_arg](const event_sub_t& sub) {
+            return (sub.secondary_cb == cb) && (sub.user_arg == user_arg);
+        }),
+            std::end(subscriptions));
     }
 
     void Unsubscribe_by_argument(void *user_arg) {
         subscriptions.erase(std::remove_if(std::begin(subscriptions),
-                                           std::end(subscriptions),
-                                           [&user_arg](const event_sub_t& sub) {
-                                               return sub.user_arg == user_arg;
-                                           }),
-                            std::end(subscriptions));
+            std::end(subscriptions),
+            [&user_arg](const event_sub_t& sub) {
+            return sub.user_arg == user_arg;
+        }),
+            std::end(subscriptions));
     }
 
     template <typename T>
-    void Subscribe_method(T *obj, void (T::*mcb)(ARGS&&...)) {
+    void Subscribe_method(T *obj, void (T::*mcb)(ARGS...)) {
         event_sub_t sub;
         sub.cb = MethodCallback<T, decltype(mcb)>;
         sub.secondary_cb = *(void**)(&mcb);
@@ -54,7 +54,7 @@ public:
     }
 
     template <typename T>
-    void Unsubscribe_method(T *obj, void (T::*mcb)(ARGS&&...)) {
+    void Unsubscribe_method(T *obj, void (T::*mcb)(ARGS...)) {
         Unsubscribe(obj, *(void**)&mcb);
     }
 
@@ -63,7 +63,7 @@ public:
         Unsubscribe_by_argument(obj);
     }
 
-    void Trigger(ARGS&&... args) {
+    void Trigger(ARGS... args) {
         for(auto& sub : subscriptions) {
             sub.cb(&sub, std::forward<ARGS>(args)...);
         }
@@ -72,22 +72,22 @@ public:
 private:
     std::vector<event_sub_t> subscriptions;
 
-    static void FunctionWithArgumentCallback(event_sub_t *sub, ARGS&&... args);
+    static void FunctionWithArgumentCallback(event_sub_t *sub, ARGS... args);
 
     template <typename T, typename MCB>
-    static void MethodCallback(event_sub_t *sub, ARGS&&... args);
+    static void MethodCallback(event_sub_t *sub, ARGS... args);
 
 };
 
 template <typename ...ARGS>
-void Event<ARGS...>::FunctionWithArgumentCallback(event_sub_t *sub, ARGS&&... args) {
+void Event<ARGS...>::FunctionWithArgumentCallback(event_sub_t *sub, ARGS... args) {
     cb_with_arg_t cb = (cb_with_arg_t)(sub->secondary_cb);
     cb(sub->user_arg, std::forward<ARGS>(args)...);
 }
 
 template <typename ...ARGS>
 template <typename T, typename MCB>
-void Event<ARGS...>::MethodCallback(event_sub_t *sub, ARGS&&... args) {
+void Event<ARGS...>::MethodCallback(event_sub_t *sub, ARGS... args) {
     MCB mcb = *(MCB*)&(sub->secondary_cb);
     T *obj = (T*)(sub->user_arg);
     (obj->*mcb)(std::forward<ARGS>(args)...);

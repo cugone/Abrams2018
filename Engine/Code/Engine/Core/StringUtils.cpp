980b01a9b7c5758526f2e9ac7072695136c110c8
cugone <cugone@gmail.com>
2019-03-08 18:05:34 -0600

    Added std::wstring overloads;

--- Engine/Code/Engine/Core/StringUtils.cpp
+++ Engine/Code/Engine/Core/StringUtils.cpp
@@ -11,6 +11,7 @@
 #include "Engine/System/Cpu.hpp"
 
 #include <cstdarg>
+#include <cwctype>
 
 #include <algorithm>
 #include <locale>
@@ -150,12 +151,45 @@ std::vector<std::string> Split(const std::string& string, char delim /*= ','*/, 
     return result;
 }
 
+
+std::vector<std::wstring> Split(const std::wstring& string, wchar_t delim /*= ','*/, bool skip_empty /*= true*/) {
+    std::size_t potential_count = 1 + std::count(string.begin(), string.end(), delim);
+    std::vector<std::wstring> result;
+    result.reserve(potential_count);
+    std::wstring curString;
+
+    std::wstringstream ss;
+    ss.str(string);
+    ss.seekg(0);
+    ss.seekp(0);
+    ss.clear();
+
+    while(std::getline(ss, curString, delim)) {
+        if(skip_empty && curString.empty()) continue;
+        result.push_back(curString);
+    }
+    result.shrink_to_fit();
+    return result;
+
+}
+
 std::pair<std::string, std::string> SplitOnFirst(const std::string& string, char delim) {
     auto eq_loc = string.find_first_of(delim);
     return std::make_pair(string.substr(0, eq_loc), string.substr(eq_loc + 1));
 }
 
+std::pair<std::wstring, std::wstring> SplitOnFirst(const std::wstring& string, wchar_t delim) {
+    auto eq_loc = string.find_first_of(delim);
+    return std::make_pair(string.substr(0, eq_loc), string.substr(eq_loc + 1));
+}
+
 std::pair<std::string, std::string> SplitOnLast(const std::string& string, char delim) {
+    auto eq_loc = string.find_last_of(delim);
+    return std::make_pair(string.substr(0, eq_loc), string.substr(eq_loc + 1));
+}
+
+
+std::pair<std::wstring, std::wstring> SplitOnLast(const std::wstring& string, wchar_t delim) {
     auto eq_loc = string.find_last_of(delim);
     return std::make_pair(string.substr(0, eq_loc), string.substr(eq_loc + 1));
 }
@@ -189,13 +223,55 @@ std::string Join(const std::vector<std::string>& strings, bool skip_empty /*= tr
     return result;
 }
 
+
+std::wstring Join(const std::vector<std::wstring>& strings, wchar_t delim, bool skip_empty /*= true*/) {
+    auto acc_op = [](const std::size_t& a, const std::wstring& b)->std::size_t { return a + static_cast<std::size_t>(1u) + b.size(); };
+    auto total_size = std::accumulate(std::begin(strings), std::end(strings), static_cast<std::size_t>(0u), acc_op);
+    std::wstring result{};
+    result.reserve(total_size);
+    for(auto iter = strings.begin(); iter != strings.end(); ++iter) {
+        if(skip_empty && (*iter).empty()) continue;
+        result += (*iter);
+        if(iter != strings.end() - 1) {
+            result.push_back(delim);
+        }
+    }
+    result.shrink_to_fit();
+    return result;
+}
+
+
+std::wstring Join(const std::vector<std::wstring>& strings, bool skip_empty /*= true*/) {
+    auto acc_op = [](const std::size_t& a, const std::wstring& b) { return a + b.size(); };
+    std::size_t total_size = std::accumulate(std::begin(strings), std::end(strings), static_cast<std::size_t>(0u), acc_op);
+    std::wstring result;
+    result.reserve(total_size);
+    for(const auto& string : strings) {
+        if(skip_empty && string.empty()) continue;
+        result += string;
+    }
+    result.shrink_to_fit();
+    return result;
+}
+
 std::string ToUpperCase(std::string string) {
     std::transform(string.begin(), string.end(), string.begin(), [](unsigned char c) -> unsigned char { return std::toupper(c, std::locale("")); });
     return string;
 }
 
+std::wstring ToUpperCase(std::wstring string) {
+    std::transform(string.begin(), string.end(), string.begin(), [](wchar_t c) -> wchar_t { return std::towupper(static_cast<std::wint_t>(c)); });
+    return string;
+}
+
 std::string ToLowerCase(std::string string) {
     std::transform(string.begin(), string.end(), string.begin(), [](unsigned char c) -> unsigned char { return std::tolower(c, std::locale("")); });
+    return string;
+}
+
+
+std::wstring ToLowerCase(std::wstring string) {
+    std::transform(string.begin(), string.end(), string.begin(), [](wchar_t c) -> wchar_t { return std::towlower(static_cast<std::wint_t>(c)); });
     return string;
 }
 
@@ -228,9 +304,21 @@ bool StartsWith(const std::string& string, const std::string& start) {
     return found_loc != std::string::npos && found_loc == 0;
 }
 
+
+bool StartsWith(const std::wstring& string, const std::wstring& start) {
+    auto found_loc = string.find(start);
+    return found_loc != std::wstring::npos && found_loc == 0;
+}
+
 bool EndsWith(const std::string& string, const std::string& end) {
     auto found_loc = string.rfind(end);
     return found_loc != std::string::npos && found_loc == string.size() - end.size();
+}
+
+
+bool EndsWith(const std::wstring& string, const std::wstring& end) {
+    auto found_loc = string.rfind(end);
+    return found_loc != std::wstring::npos && found_loc == string.size() - end.size();
 }
 
 //From https://stackoverflow.com/a/3418285/421178
@@ -246,6 +334,18 @@ std::string ReplaceAll(std::string string, const std::string& from, const std::s
     return string;
 }
 
+
+std::wstring ReplaceAll(std::wstring string, const std::wstring& from, const std::wstring& to) {
+    if(from.empty()) {
+        return string;
+    }
+    std::size_t start_pos = 0;
+    while((start_pos = string.find(from, start_pos)) != std::wstring::npos) {
+        string.replace(start_pos, from.size(), to);
+        start_pos += to.size(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+    }
+    return string;
+}
 
 std::vector<std::size_t> FindAll(std::string string, const char c) {
     std::vector<std::size_t> results{};
@@ -266,9 +366,34 @@ std::vector<std::size_t> FindAll(std::string string, const std::string& sequence
     return results;
 }
 
+std::vector<std::size_t> FindAll(std::wstring string, const wchar_t c) {
+    std::vector<std::size_t> results{};
+    std::size_t offset = 0;
+    while((offset = string.find(c, offset)) != std::wstring::npos) {
+        results.push_back(offset++);
+    }
+    return results;
+}
+
+std::vector<std::size_t> FindAll(std::wstring string, const std::wstring& sequence) {
+    std::vector<std::size_t> results{};
+    std::size_t offset = 0;
+    while((offset = string.find(sequence, offset)) != std::wstring::npos) {
+        results.push_back(offset);
+    }
+    return results;
+}
+
 std::string TrimWhitespace(std::string string) {
     auto first_non_space = string.find_first_not_of(" \r\n\t\v\f");
     auto last_non_space = string.find_last_not_of(" \r\n\t\v\f");
+    return string.substr(first_non_space, last_non_space - first_non_space + 1);
+}
+
+
+std::wstring TrimWhitespace(std::wstring string) {
+    auto first_non_space = string.find_first_not_of(L" \r\n\t\v\f");
+    auto last_non_space = string.find_last_not_of(L" \r\n\t\v\f");
     return string.substr(first_non_space, last_non_space - first_non_space + 1);
 }
 
--- Engine/Code/Engine/Core/StringUtils.hpp
+++ Engine/Code/Engine/Core/StringUtils.hpp
@@ -26,23 +26,40 @@ const std::string Stringf(const char* format, ...);
 const std::string Stringf(const int maxLength, const char* format, ...);
 
 std::vector<std::string> Split(const std::string& string, char delim = ',', bool skip_empty = true);
+std::vector<std::wstring> Split(const std::wstring& string, wchar_t delim = ',', bool skip_empty = true);
 std::pair<std::string, std::string> SplitOnFirst(const std::string& string, char delim);
+std::pair<std::wstring, std::wstring> SplitOnFirst(const std::wstring& string, wchar_t delim);
 std::pair<std::string, std::string> SplitOnLast(const std::string& string, char delim);
+std::pair<std::wstring, std::wstring> SplitOnLast(const std::wstring& string, wchar_t delim);
 std::string Join(const std::vector<std::string>& strings, char delim, bool skip_empty = true);
+std::wstring Join(const std::vector<std::wstring>& strings, wchar_t delim, bool skip_empty = true);
 std::string Join(const std::vector<std::string>& strings, bool skip_empty = true);
+std::wstring Join(const std::vector<std::wstring>& strings, bool skip_empty = true);
 
 std::string ToUpperCase(std::string string);
+std::wstring ToUpperCase(std::wstring string);
 std::string ToLowerCase(std::string string);
+std::wstring ToLowerCase(std::wstring string);
 
 std::string ConvertUnicodeToMultiByte(const std::wstring& unicode_string);
-std::wstring ConvertMultiByteToUnicode(const std::string& filePath);
+std::wstring ConvertMultiByteToUnicode(const std::string& multi_byte_string);
 
 bool StartsWith(const std::string& string, const std::string& start);
+bool StartsWith(const std::wstring& string, const std::wstring& start);
+
 bool EndsWith(const std::string& string, const std::string& end);
+bool EndsWith(const std::wstring& string, const std::wstring& end);
+
 std::string ReplaceAll(std::string string, const std::string& from, const std::string& to);
+std::wstring ReplaceAll(std::wstring string, const std::wstring& from, const std::wstring& to);
+
 std::vector<std::size_t> FindAll(std::string string, const char c);
+std::vector<std::size_t> FindAll(std::wstring string, const wchar_t c);
 std::vector<std::size_t> FindAll(std::string string, const std::string& sequence);
+std::vector<std::size_t> FindAll(std::wstring string, const std::wstring& sequence);
+
 std::string TrimWhitespace(std::string string);
+std::wstring TrimWhitespace(std::wstring string);
 
 constexpr const uint32_t FourCC(const char* id) {
     return static_cast<uint32_t>((((id[0] << 24) & 0xFF000000) | ((id[1] << 16) & 0x00FF0000) | ((id[2] << 8) & 0x0000FF00) | ((id[3] << 0) & 0x000000FF)));

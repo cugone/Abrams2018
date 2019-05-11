71f6a7283af559da95e46a48b79a972fc0eafa86
cugone <cugone@gmail.com>
2019-05-10 23:18:41 -0500

    Add tangent and bitangent

--- Engine/Code/Engine/Core/Vertex3D.hpp
+++ Engine/Code/Engine/Core/Vertex3D.hpp
@@ -10,7 +10,7 @@ class InputLayout;
 class Vertex3D {
 public:
 
-    Vertex3D(const Vector3& pos = Vector3::ZERO, const Rgba& color = Rgba::White, const Vector2& tex_coords = Vector2::ZERO, const Vector3& normal = Vector3::Z_AXIS);
+    Vertex3D(const Vector3& pos = Vector3::ZERO, const Rgba& color = Rgba::White, const Vector2& tex_coords = Vector2::ZERO, const Vector3& normal = Vector3::Z_AXIS, const Vector3& tangent = Vector3::ZERO, const Vector3& bitangent = Vector3::ZERO);
     Vertex3D(const Vertex3D& other) = default;
     Vertex3D(Vertex3D&& other) = default;
     Vertex3D& operator=(const Vertex3D& other) = default;
@@ -19,6 +19,8 @@ public:
     Vector4 color = Rgba::White.GetRgbaAsFloats();
     Vector2 texcoords = Vector2::ZERO;
     Vector3 normal = Vector3::Z_AXIS;
+    Vector3 tangent = Vector3::ZERO;
+    Vector3 bitangent = Vector3::ZERO;
 
 protected:
 private:
@@ -27,11 +29,15 @@ private:
 inline Vertex3D::Vertex3D(const Vector3& pos /*= Vector3::ZERO*/
                           , const Rgba& color /*= Rgba::WHITE*/
                           , const Vector2& tex_coords /*= Vector2::ZERO*/
-                          , const Vector3& normal /*= Vector3::Z_AXIS*/)
+                          , const Vector3& normal /*= Vector3::Z_AXIS*/
+                          , const Vector3& tangent /* Vector3::ZERO */
+                          , const Vector3& bitangent /* Vector3::ZERO */)
     : position(pos)
     , color(color.GetRgbaAsFloats())
     , texcoords(tex_coords)
     , normal(normal)
+    , tangent(tangent)
+    , bitangent(bitangent)
 {
     /* DO NOTHING */
 }

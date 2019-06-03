7e6ef59f23b9698e0723c6a2cff243d360d65f05
cugone <cugone@gmail.com>
2019-06-02 21:17:48 -0500

    clamp trauma to 0.0 to 1.0

--- Engine/Code/Engine/Renderer/Camera2D.cpp
+++ Engine/Code/Engine/Renderer/Camera2D.cpp
@@ -29,6 +29,7 @@ void Camera2D::CalcViewMatrix() {
 
 void Camera2D::Update(TimeUtils::FPSeconds deltaSeconds) {
     trauma -= trauma_recovery_rate * deltaSeconds.count();
+    trauma = std::clamp(trauma, 0.0f, 1.0f);
 }
 
 const Vector2& Camera2D::GetPosition() const {
